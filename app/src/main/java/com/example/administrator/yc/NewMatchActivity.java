b70cf32860fbbe367ebbcd8679a84d0747d296aa
YU <772381494@qq.com>
2018-03-07 15:47:33 +0800

    2.4
    增加地图页面跳转

--- app/src/main/java/com/example/administrator/yc/NewMatchActivity.java
+++ app/src/main/java/com/example/administrator/yc/NewMatchActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.yc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -33,6 +34,8 @@ public class NewMatchActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //设置跳转到GPS页面
+                Intent intent=new Intent(NewMatchActivity.this,MapActivity.class);
+                startActivity(intent);
             }
         });
     }
