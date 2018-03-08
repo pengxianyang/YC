946833e6a1689374591329c664fc47771f39cd4d
pengxianyang <32325412+pengxianyang@users.noreply.github.com>
2018-03-08 18:24:26 +0800

    修改布局

--- app/src/main/java/com/example/administrator/yc/ContactviewActivity.java
+++ app/src/main/java/com/example/administrator/yc/ContactviewActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.WindowManager;
 
 public class ContactviewActivity extends Activity {
     private RecyclerView contactList;
@@ -16,8 +17,9 @@ public class ContactviewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_contactview);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 //这里加载该用户的好友信息
-        contactNames = new String[] {"彭先洋","潘国秋","欧欣祺","马东辉","刘顺宇"};
+        contactNames = new String[] {"Alice","Bob","Candy","Doge","Edison"};
         contactList = (RecyclerView) findViewById(R.id.contact_list);
         letterView = (LetterView) findViewById(R.id.letter_view);
         layoutManager = new LinearLayoutManager(this);
